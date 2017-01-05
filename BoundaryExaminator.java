04095c178a4bcf8304e3cad9b1cf2e236cbc25d4
Dor <hmalings@yahoo.co.uk>
2017-01-05 14:23:10 +0000

    Update BoundaryExaminator.java

--- BoundaryExaminator.java
+++ BoundaryExaminator.java
@@ -1,3 +1,13 @@
+/*
+This is a small program which is intended to test the users knowledge of the /b and /B metacharacters.
+
+The user is shown a random String of 8 characters and must type in where /b or /B occur.
+
+This program is written because I have a massive blind spot about /b and /B and would really like to not fail my OCP exam because of it.
+
+*/
+
+
 public class BoundaryExaminator{
 
 	private char[] charArray; //ArrayList?
